d936000d9017df575858c195aaedb42714212dec
Hamo <hamo1992.1992@gmail.com>
2019-08-23 00:16:36 +0400

    commit from home

--- app/urls.py
+++ app/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.exchange_rates_by_date, name='my_soap_app'),
-    re_path(r'^convert/', views.exchange_rates, name='exchange_rates')
+    #re_path(r'^convert/', views.exchange_rates, name='exchange_rates')
 ]
--- app/views.py
+++ app/views.py
@@ -21,10 +21,10 @@ def convert_money(value, rate1, amount1, rate2, amount2):
     rate2 = float(rate2)
     amount2 = float(amount2)
 
-    r1 = rate1 / amount1
-    r2 = rate2 / amount2
-    if r2 != 0:
-        return value * r1 / r2
+    num1 = rate1 / amount1
+    num2 = rate2 / amount2
+    if num2 != 0:
+        return round(value * num1 / num2, 4)
     else:
         return "Rate_2 is 0.0000"
 
@@ -39,21 +39,18 @@ def exchange_rates_by_date(request):
     difference = {}
     convert_value = ''
 
-    if request.GET:
-        data = Exchange.objects.last()
-        iso = data.iso
-        amount = data.amount
-        rate = data.rate
-        difference = data.differnce
+    # data = Exchange.objects.last()
+    # iso = data.iso
+    # print('ISO =', iso)
+    # print('Exchange_rates_by_date_by_iso',exchange_rates_by_date_by_iso(date, 'USD'))
 
-    elif request.POST:
+    if request.POST:
         if request.POST.get('date'):
-            date = datetime.strptime(request.POST.get('date'), "%Y-%m-%d")
+            date = request.POST.get('date') #datetime.strptime(request.POST.get('date'), "%Y-%m-%d")
             client = Client('http://api.cba.am/exchangerates.asmx?wsdl')
             result = client.service.ExchangeRatesByDate(date)
 
             list_rates = list(result.Rates.ExchangeRate)
-
             for i, val in enumerate(list_rates, 1):
                 data[i] = val
 
@@ -66,32 +63,29 @@ def exchange_rates_by_date(request):
             for val in data.values():
                 difference[val.ISO] = val.Difference
 
-            d = Exchange(iso, amount, rate, difference, date)
-            d.save()
+            exchange = Exchange(iso=iso, amount=amount, rate=rate, difference=difference, date=date)
+            exchange.save()
 
         elif request.POST.get('number'):
-            post = request.POST
             query = Exchange.objects.last()
-
-            date = query.date
 
             iso = str(query.iso[1:-1]).replace("'", "").split(', ')
             amount = str_to_dict(query.amount)
             rate = str_to_dict(query.rate)
             difference = str_to_dict(query.difference)
 
-            iso_from = post.get('select_from')
-            iso_to = post.get('select_to')
-
+            iso_from = request.POST.get('select_from')
+            iso_to = request.POST.get('select_to')
             amount_from = amount.get(iso_from)
             amount_to = amount.get(iso_to)
             rate_from = rate.get(iso_from)
             rate_to = rate.get(iso_to)
+            number = request.POST.get('number')
 
-            number = post.get('number')
+            convert_value = convert_money(number, rate_from, amount_from, rate_to, amount_to)
 
             print("=========================================== START =================================================")
-            print(f'num = {number}')
+            print(f'number = {number}')
             print(f'iso_from = {iso_from}')
             print(f'iso_to = {iso_to}')
             print(f'amount_from = {amount_from}')
@@ -100,8 +94,7 @@ def exchange_rates_by_date(request):
             print(f'rate_to = {rate_to}')
             print("================================================ END ==============================================")
 
-            convert_value = convert_money(number, rate_from, amount_from, rate_to, amount_to)
-
+    print('date=', date)
     context = {
         'date': date,
         'data': data,
@@ -113,3 +106,9 @@ def exchange_rates_by_date(request):
     }
 
     return render(request, 'soap.html', context)
+
+
+def exchange_rates_by_date_by_iso(date, iso):
+    client = Client('http://api.cba.am/exchangerates.asmx?wsdl')
+    result = client.service.ExchangeRatesByDateByISO(date, iso)
+    return result
